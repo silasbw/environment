ef360715baae0adf1926a6525060298939a6d85c
Silas Boyd-Wickizer <silasbw@gmail.com>
2018-10-28 11:35:58 -0700

    run-dmw.sh tweaks

--- run-dwm.sh
+++ run-dwm.sh
@@ -15,8 +15,9 @@ stoken-gui &
 
 while true
 do
+    vpnc=$(if [ -f /var/run/vpnc.pid ]; then echo ' vpnc'; else echo ''; fi)
     battery=$(upower -i $(upower -e | grep BAT) | grep --color=never -E "percentage" | awk '{print $2}')
-    status="$(date +'%a %b %e %l:%M') $battery"
+    status="$(date +'%a %b %e %l:%M') $battery $vpnc"
     xsetroot -name "$status"
     sleep 1
 done &
